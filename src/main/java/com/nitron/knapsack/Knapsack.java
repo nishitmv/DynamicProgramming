f5a93777975c859cc35b5d73654591f31d9bcbbd
nishit <nishitmv@gmail.com>
2022-04-26 00:30:52 +0530

    DP code

--- src/main/java/com/nitron/knapsack/Knapsack.java
+++ src/main/java/com/nitron/knapsack/Knapsack.java
@@ -3,34 +3,24 @@ package com.nitron.knapsack;
 import java.util.Arrays;
 
 public class Knapsack {
-    int[][] matrix ;
+    Integer[][] matrix;
+
     public Knapsack(int size, int n) {
-        matrix = new int[n+1][size+1];
-        for(int[] arr : matrix)
-       Arrays.fill(arr, -1);
+        matrix = new Integer[n + 1][size + 1];
     }
-    public int fillKnapsack(int[] weights, int[] values, int size, int n)
-    {
-        if(size ==0 || n ==0)
+
+    public int fillKnapsack(int[] weights, int[] values, int size, int n) {
+        if (size == 0 || n == 0)
             return 0;
+        if (matrix[n][size] != null)
+            return matrix[n][size];
 
-        if(weights[n-1]<=size) {
-            int val;
-            if (matrix[n][size] != -1) {
-                val = matrix[n][size];
-                System.out.println("Found : "+val);
-            }
-                else {
-                val = Math.max(values[n - 1] + fillKnapsack(weights, values, size - weights[n - 1], n - 1), fillKnapsack(weights, values, size, n - 1));
-                matrix[n][size] = val;
-            }
-            return val;
-        }
-        else{
-            int val = fillKnapsack(weights, values, size, n-1);
-            matrix[n][size] = val;
-            return val;
+        if (weights[n - 1] <= size) {
+            return matrix[n][size] = Math.max(values[n - 1] + fillKnapsack(weights, values, size - weights[n - 1], n - 1), fillKnapsack(weights, values, size, n - 1));
+
+        } else {
+            return matrix[n][size] = fillKnapsack(weights, values, size, n - 1);
+
         }
     }
-
 }
