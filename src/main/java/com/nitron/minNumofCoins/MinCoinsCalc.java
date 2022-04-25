41801e2a7b9c982fc72c89db0d84d78e7fb4ea37
nishit <nishitmv@gmail.com>
2022-04-26 00:33:35 +0530

    DP code

--- src/main/java/com/nitron/minNumofCoins/MinCoinsCalc.java
+++ src/main/java/com/nitron/minNumofCoins/MinCoinsCalc.java
@@ -2,14 +2,14 @@ package com.nitron.minNumofCoins;
 
 public class MinCoinsCalc {
 
-    int[][] matrix ;
+    Integer[][] matrix ;
     public int coinChange(int[] coins, int amount) {
         if(amount==0)
             return 0;
         if(coins.length==1)
             return amount%coins[0]==0 ? amount/coins[0] : -1;
         int n = coins.length;
-        matrix = new int[n+1][amount+1];
+        matrix = new Integer[n+1][amount+1];
 
         for(int i=0;i<n+1;i++)
             for(int j=0;j<amount+1;j++) {
@@ -23,8 +23,6 @@ public class MinCoinsCalc {
                     else
                         matrix[i][j] = Integer.MAX_VALUE - 1;
                 }
-                else matrix[i][j] = -1;
-
             }
         int result =calculateMinCoins(coins, n, amount);
         return result == Integer.MAX_VALUE - 1 ? -1 :result;
@@ -36,17 +34,12 @@ public class MinCoinsCalc {
             return 0;
         if(n==0)
             return 1;
-
+        if(matrix[n][sum]!=null)
+            return matrix[n][sum];
         if(coins[n-1]<=sum )
         {
-            if(matrix[n][sum]!=-1)
-                return matrix[n][sum];
-            else
                 return matrix[n][sum] = Math.min(1+calculateMinCoins(coins, n, sum - coins[n-1]), calculateMinCoins(coins, n-1, sum));
         }
-        else
-        if(matrix[n][sum]!=-1)
-            return matrix[n][sum];
         else
             return matrix[n][sum] = calculateMinCoins(coins, n-1, sum);
 
