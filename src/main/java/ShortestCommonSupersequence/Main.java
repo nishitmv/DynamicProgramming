d7259d5a629b2605b2a0cf305c0c3b7a2d4c5b9c
nishit <nishitmv@gmail.com>
2022-04-27 17:20:41 +0530

    DP code

---
+++ src/main/java/ShortestCommonSupersequence/Main.java
@@ -0,0 +1,15 @@
+package ShortestCommonSupersequence;
+
+import com.nitron.printingLCS.LCSPrinter;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String str1 = "AGGTABS";
+        String str2 = "GXTXAYB" ;
+
+
+       new ShortestCommonSuperSeq().calcShortestCommonSuperSeq(str1.toCharArray(), str2.toCharArray(), str1.length(), str2.length());
+    }
+}
