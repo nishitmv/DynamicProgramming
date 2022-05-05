4f7e0b9a8557bd5a2b16cf3955a62eabc64f89fd
Nishit <nishit.vishwanath@tesco.com>
2022-05-05 19:43:28 +0530

    DP - MCM and Palindrome partition

--- src/main/java/com/nitron/MatrixChainMultiplication/Main.java
+++ src/main/java/com/nitron/MatrixChainMultiplication/Main.java
@@ -1,14 +1,13 @@
 package com.nitron.MatrixChainMultiplication;
 
-import com.nitron.MatrixChainMultiplicationFORMAT.MatrixChainMultiFormat;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int[] arr = {10,30,5,60};
+        int[] arr = {10,30,5,60, 20, 40};
 
-        System.out.println(new MatrixChainMulti().calc(arr, 1, arr.length-1));
+        System.out.println(new MatrixChainMulti().calc(arr, 1, arr.length-1, 1000));
 
     }
 
