338352cb5da2c8439c8e87c6c8f230c63452314c
nishit <nishitmv@gmail.com>
2022-05-06 14:10:39 +0530

    DP Boolean parenthesization memoized

--- src/main/java/com/nitron/BooleanParenthesization/BooleanParenthesizer.java
+++ src/main/java/com/nitron/BooleanParenthesization/BooleanParenthesizer.java
@@ -4,9 +4,11 @@ public class BooleanParenthesizer {
 
     private static final char TRUE = 'T';
     private static final char FALSE = 'F';
-
+    Integer[][] trueMatrix;
+    Integer[][] falseMatrix;
     public int calcWaysToParenthesize(String expr) {
-
+        trueMatrix = new Integer[expr.length()][expr.length()];
+        falseMatrix =  new Integer[expr.length()][expr.length()];
         return calcWaysToParenthesize(expr, 0, expr.length() - 1, true);
 
     }
@@ -17,11 +19,16 @@ public class BooleanParenthesizer {
             return 0;
         if (i == j) {
             if (isTrue)
-               return  expr.charAt(i) == TRUE ?  1 : 0;
+                return  expr.charAt(i) == TRUE ?  1 : 0;
             else
                 return expr.charAt(i) == FALSE ?  1 : 0;
         }
-            int ans =0;
+        if(isTrue&& trueMatrix[i][j]!=null)
+            return trueMatrix[i][j];
+        if(!isTrue&& falseMatrix[i][j]!=null)
+            return falseMatrix[i][j];
+
+        int ans =0;
             for(int k =i+1; k<j; k+=2) // +2 as we need to skip chars (T/F) and hit operators, k is always operator . i+1 as 1st char is not operator .
             {
                 int leftTrue = calcWaysToParenthesize(expr, i, k-1, true);
@@ -30,7 +37,12 @@ public class BooleanParenthesizer {
                 int rightFalse =  calcWaysToParenthesize(expr, k+1, j, false);
                 ans = ans + evaluate(expr.charAt(k), leftTrue, leftFalse, rightTrue, rightFalse, isTrue);
             }
-            return ans;
+
+        if(isTrue)
+            trueMatrix[i][j] = ans;
+        else
+            falseMatrix[i][j] = ans;
+        return ans;
     }
 
     private int evaluate(char operator, int leftTrue, int leftFalse, int rightTrue, int rightFalse, boolean isTrue) {
--- src/main/java/com/nitron/BooleanParenthesization/Main.java
+++ src/main/java/com/nitron/BooleanParenthesization/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String expr = "T&F^T|F";
+        //String expr = "T&F^T|F";
+        String expr = "T|F^F&T|F^F^F^T|T&T^T|F^T^F&F^T|T^F";
 
         System.out.println(new BooleanParenthesizer().calcWaysToParenthesize(expr));
 
