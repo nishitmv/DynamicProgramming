3077c2a0bfcf5085b95719cdc4034d54d1eb2df7
nishit <nishitmv@gmail.com>
2022-05-08 23:17:19 +0530

    DP Egg DroppingProblem

---
+++ src/main/java/com/nitron/eggDroppingProblem/Main.java
@@ -0,0 +1,11 @@
+package com.nitron.eggDroppingProblem;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int f = 36;
+        int e = 2;
+
+        System.out.println(new EggDroppingProblemSolver().solve(f,e));
+    }
+}
