095e261184127bcb8ad01633c8b11be6483e6d80
nishit <nishitmv@gmail.com>
2022-05-13 16:58:31 +0530

    Kadanes Max Subarray Sum

--- src/main/java/com/nitron/KadanesMaxSubArray/Main.java
+++ src/main/java/com/nitron/KadanesMaxSubArray/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         int[] arr = {5,4,-1,7,8};
-        var maxSubArrCalc = new MaxSubArrayCalc();
-        maxSubArrCalc.calcMaxSubArraySum(arr);
-        System.out.println(maxSubArrCalc.getSum());
+      //  var maxSubArrCalc = new MaxSubArrayCalc();
+       // maxSubArrCalc.calcMaxSubArraySum(arr);
+        System.out.println(new KadanesMaxSubArrayCalc().calcMaxSubArray(arr));
 
     }
 
