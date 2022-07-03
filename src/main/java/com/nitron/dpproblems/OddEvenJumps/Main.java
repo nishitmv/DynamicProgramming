e8712650568c6b82a9ca999c6cba69b85b8cd3e2
nishit <nishitmv@gmail.com>
2022-07-04 01:44:04 +0530

    Odd Even Jumps . Hard . Using TreeMap and highercache and lowercache

---
+++ src/main/java/com/nitron/dpproblems/OddEvenJumps/Main.java
@@ -0,0 +1,15 @@
+package com.nitron.dpproblems.OddEvenJumps;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10,13,12,14,15};
+        int[] arr2 = {2,3,1,1,4};
+        int[] arr3 ={5,1,3,4,2};
+
+                System.out.println(new OddEvenJumps().countJumps(arr3));
+
+    }
+
+}
---
+++ src/main/java/com/nitron/dpproblems/OddEvenJumps/OddEvenJumps.java
@@ -0,0 +1,60 @@
+package com.nitron.dpproblems.OddEvenJumps;
+
+import java.util.TreeMap;
+
+public class OddEvenJumps {
+
+    boolean[] higherCache;
+    boolean[] lowerCache;
+    int count =1;
+
+    public int countJumps(int[] arr) {
+
+        int n = arr.length;
+        higherCache = new boolean[n];
+        lowerCache = new boolean[n];
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        higherCache[n-1] =true;
+        lowerCache[n-1] =true;
+
+        map.put(arr[n-1], n-1);
+
+        for(int i=n-2;i>=0;i--) {
+            if( countJumps(arr, i, n - 1, map, true))
+                count++;
+            // Do the countJumps for lower so that lowerCache has data for i, else it's an issue as we use ceil
+            // and it gets stuck on same node and gives incorrect results.
+            countJumps(arr, i, n - 1, map, false);
+
+            map.put(arr[i], i);
+        }
+        return count;
+    }
+
+    private boolean countJumps(int[] arr, int i, int n, TreeMap<Integer, Integer> map, Boolean higher) {
+
+
+        if( higher) {
+            if(higherCache[i]) {
+
+                return true;
+            }
+            var higherValue = map.ceilingEntry(arr[i]);
+            if (higherValue != null && higherValue.getValue()>i) {
+                return higherCache[i] = countJumps(arr, higherValue.getValue(), n, map, false);
+            }
+        }
+        if( !higher) {
+            if(lowerCache[i]) {
+
+                return true;
+            }
+            var lowerValue = map.floorEntry(arr[i]);
+            if (lowerValue != null && lowerValue.getValue()>i) {
+                return lowerCache[i] = countJumps(arr, lowerValue.getValue(), n, map, true);
+            }
+        }
+
+        return i == n;
+    }
+}
