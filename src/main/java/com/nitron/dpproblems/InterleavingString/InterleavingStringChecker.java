c753b67613ec22a5bda14947aa64bdb9e82de992
nishit <nishitmv@gmail.com>
2022-07-08 01:06:20 +0530

    Interleaving Sequence

---
+++ src/main/java/com/nitron/dpproblems/InterleavingString/InterleavingStringChecker.java
@@ -0,0 +1,45 @@
+package com.nitron.dpproblems.InterleavingString;
+
+public class InterleavingStringChecker {
+
+    Boolean[][]  matrix;
+    public boolean isInterleave(String s1, String s2, String s3) {
+
+        int m = s3.length();
+
+        if( (s1.length() == 0 && s2.length() ==0)  && m!=0)
+            return false;
+        if(s1.length()>m || s2.length()>m)
+            return false;
+        if(s1.length()+ s2.length() >m)
+            return false;
+        matrix = new Boolean[s1.length()+1][s2.length()+1];
+
+        return isInterleave(s1, 0, s2, 0, s3);
+
+    }
+
+    private boolean isInterleave(String s1, int i, String s2, int j, String s3) {
+
+        if(i+j == s3.length())
+            return true;
+
+        if(matrix[i][j] != null)
+            return matrix[i][j];
+
+        if(i<s1.length() && s3.charAt(i+j) == s1.charAt(i))
+        {
+             matrix[i][j] = isInterleave(s1, i+1, s2, j, s3);
+             if(matrix[i][j])
+                 return true;
+        }
+        if(j<s2.length() && s3.charAt(i+j) == s2.charAt(j))
+        {
+             matrix[i][j] = isInterleave(s1, i, s2, j+1, s3);
+            if(matrix[i][j])
+                return true;
+        }
+        return matrix[i][j] = false;
+
+    }
+}
---
+++ src/main/java/com/nitron/dpproblems/InterleavingString/Main.java
@@ -0,0 +1,16 @@
+package com.nitron.dpproblems.InterleavingString;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac";
+
+
+        System.out.println(new InterleavingStringChecker().isInterleave(s1, s2, s3));
+
+
+    }
+
+
+}
